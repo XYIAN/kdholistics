17ad04f939f7e9b260aad4883cccbf58a536e3af
XYIAN <kylexyiandilbeck@gmail.com>
2025-01-07 21:36:05 -0800

    update encoding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
 "use client";
+import KDHBanner from "@/components/common/KDHBanner";
 import { WRAPPER_STYLE_DEFAULT } from "@/constants/WRAPPER_STYLE_DEFAULT";
-import { Image } from "primereact/image";
 
 export default function Home() {
   return (
     <div className={WRAPPER_STYLE_DEFAULT}>
-      <div>
-        {/* <div>KD Holistic Health</h1> */}
-        <Image
-          src={
-            "https://github.com/XYIANSoftware/images/blob/main/client/kdh/kd1.png?raw=true"
-          }
-          alt="temp-logo"
-          style={{ width: "200px !important" }}
-        />
-      </div>
+      <KDHBanner
+        title="KD's Holistic Health"
+        imgSrc="https://github.com/XYIANSoftware/images/blob/main/client/kdh/kd1.png?raw=true"
+        description="Empowering Mind, Body, and Soul – Your Journey to Wholeness Starts Here!"
+      />
     </div>
   );
 }
