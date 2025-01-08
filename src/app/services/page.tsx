334d702576a0449af63dde8b69a2c32c757a8422
XYIAN <kylexyiandilbeck@gmail.com>
2025-01-07 23:29:52 -0800

    menu fix

---
+++ src/app/services/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      <h1>KDH Services</h1>
+    </div>
+  );
+};
+
+export default Services;
--- src/constants/MAIN_MENU.ts
+++ src/constants/MAIN_MENU.ts
@@ -2,8 +2,8 @@ import { MenuItemModel } from "@/types";
 
 export const MENU: MenuItemModel[] = [
   { label: "Home", route: "" },
-  { label: "Services", route: "/services" },
-  { label: "Events", route: "/events" },
-  { label: "About KDH", route: "/about" },
-  { label: "Get in Touch!", route: "/social" },
+  { label: "Services", route: "services" },
+  { label: "Events", route: "events" },
+  { label: "About KDH", route: "about" },
+  { label: "Get in Touch!", route: "social" },
 ];
