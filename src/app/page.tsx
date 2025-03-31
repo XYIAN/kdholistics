79e177ded38ffbb97e6343d0ab1fd8a34912652f
XYIAN <kylexyiandilbeck@gmail.com>
2025-03-31 11:27:51 -0700

    social page and home background fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,18 @@ import HeroImage from "../../public/images/2.png";
 import EmailSignup from "@/features/home/EmailSignup";
 import MoonCirclesPreview from "@/features/home/MoonCirclesPreview";
 import MoonCircleScrollPreview from "@/features/home/MoonCircleScrollPreview";
+import "@/styles/_homePage.scss";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  const handleNavigateClick = (to: string) => {
+    router.push(`/${to}`);
+  };
+
   return (
-    <div>
+    <div className="home-container">
       <Hero
         title="Welcome to Holistic Kas"
         description="A space for personal and spiritual growth through holistic healing, education, and community."
@@ -15,7 +24,7 @@ export default function Home() {
         ctaIcon="pi pi-heart"
         imageSrc={HeroImage.src}
         imageAlt="Kas Profile"
-        onCtaClick={() => console.log("CTA clicked")}
+        onCtaClick={() => handleNavigateClick("/social")}
       />
       <EmailSignup />
       <MoonCirclesPreview />
