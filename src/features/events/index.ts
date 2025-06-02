fb027281bfa76ff8fa9cb11aa27ea4c113b71101
XYIAN <kylexyiandilbeck@gmail.com>
2025-06-01 21:39:52 -0700

    add home page imports

--- src/features/events/index.ts
+++ src/features/events/index.ts
@@ -1,3 +1,4 @@
-export * from "./EventItem";
-export * from "./EventpreviewCard";
+export { default as EventItem } from "./EventItem";
+export { default as EventPreviewCard } from "./EventpreviewCard";
+export { default as EventsPage } from "./EventsPage";
 export * from "./moonCircles";
